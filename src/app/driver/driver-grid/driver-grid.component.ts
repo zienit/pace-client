befb506d92ae52d2275cefbc49c6e28dfa9449e9
Zienit <4760167+zienit@users.noreply.github.com>
2019-09-26 00:34:20 +0200

    async ngFor

--- src/app/driver/driver-grid/driver-grid.component.ts
+++ src/app/driver/driver-grid/driver-grid.component.ts
@@ -3,7 +3,7 @@ import { DriverService } from '../driver.service';
 import { Driver } from '../driver.model';
 import { PageEvent } from '@angular/material';
 import { MediaObserver } from '@angular/flex-layout';
-import { Observable, Subscription } from 'rxjs';
+import { Observable, Subscription, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -13,7 +13,8 @@ import { map } from 'rxjs/operators';
 })
 export class DriverGridComponent implements OnInit, OnDestroy {
 
-  drivers: Driver[];
+  allDrivers: Driver[];
+  drivers: BehaviorSubject<Driver[]>;
   filters: { name: string, selected: boolean }[];
   pageIndex: number = 0;
   pageSize: number = 0;
@@ -36,30 +37,39 @@ export class DriverGridComponent implements OnInit, OnDestroy {
           return 4;
       }
     }));
-    this.drivers = this.driverService.getDrivers();
-    const countries: string[] = this.drivers.reduce(
-      (countries, driver) => countries.includes(driver.country) ? countries : [...countries, driver.country],
-      []
-    );
-    this.filters = countries.sort().map(c => { return { name: c, selected: false }; });
+    this.allDrivers = this.driverService.getDrivers();
+    this.drivers = new BehaviorSubject<Driver[]>(this.allDrivers);
+    // const countries: string[] = this.drivers.reduce(
+    //   (countries, driver) => countries.includes(driver.country) ? countries : [...countries, driver.country],
+    //   []
+    // );
+    // this.filters = countries.sort().map(c => { return { name: c, selected: false }; });
 
     // when column count change, reset the pagination so that item currenly in top-left position remains visible.
     this.resetPagination = this.columnCount.subscribe(cc => {
       if (cc == 1) {
-        // paginator is removed
+        // paginator is removed from DOM
         this.pageIndex = 0;
-        this.pageSize = this.drivers.length;
+        this.pageSize = this.drivers.value.length;
 
       } else {
         this.pageIndex = Math.floor((this.pageIndex * this.pageSize) / (cc * 2));
         this.pageSize = cc * 2;
       }
+      this.drivers.next(this.pagedDrivers());
     })
   }
 
   onPage(event: PageEvent) {
     this.pageIndex = event.pageIndex;
     this.pageSize = event.pageSize;
+    this.drivers.next(this.pagedDrivers());
+  }
+
+  private pagedDrivers() {
+    const from = this.pageIndex * this.pageSize;
+    const to = from + this.pageSize;
+    return this.allDrivers.slice(from, to);
   }
 
   ngOnDestroy() {
