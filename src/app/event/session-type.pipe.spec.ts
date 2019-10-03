6e42a35feaebe22679fb2c964fc71e3465753cc3
Zienit <4760167+zienit@users.noreply.github.com>
2019-10-03 23:52:13 +0200

    stuff

--- src/app/event/session-type.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SessionTypePipe } from './session-type.pipe';
-
-describe('SessionTypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SessionTypePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
