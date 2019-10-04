a5036552c28b698e91ce743383615eabd559a8b0
Zienit <4760167+zienit@users.noreply.github.com>
2019-10-04 23:37:14 +0200

    schedule complete

--- src/app/common/flag/flag.component.ts
+++ src/app/common/flag/flag.component.ts
@@ -17,19 +17,23 @@ export class FlagComponent implements OnInit {
     'esp': { row: 0, col: 5 },
     'fra': { row: 0, col: 6 },
     'aus': { row: 0, col: 7 },
-    'jap': { row: 0, col: 8 },
+    'jpn': { row: 0, col: 8 },
     'can': { row: 0, col: 9 },
     'bel': { row: 1, col: 0 },
     'nld': { row: 1, col: 1 },
     'che': { row: 1, col: 2 },
+    'bra': { row: 1, col: 4 },
     'chn': { row: 1, col: 5 },
     'rus': { row: 1, col: 8 },
     'aut': { row: 1, col: 9 },
     'mex': { row: 2, col: 1 },
+    'are': { row: 2, col: 3 },
+    'hun': { row: 2, col: 5 },
     'mco': { row: 2, col: 6 },
     'bhr': { row: 2, col: 7 },
     'tha': { row: 3, col: 3 },
     'fin': { row: 3, col: 7 },
+    'sgp': { row: 3, col: 9 },
     'aze': { row: 4, col: 4 },
     'pol': { row: 4, col: 5 },
     'dnk': { row: 6, col: 6 }
@@ -39,7 +43,7 @@ export class FlagComponent implements OnInit {
     return {
       'background-image': 'url(https://us-east-1-cdn-ui.motorsport.com/817/design/dist/components/flags/flags-50x30/flags-100x60-1_466a02afa94e0eb8371f35e15bb7583d.png)',
       'height': '100%',
-      'background-size':'1000%',
+      'background-size': '1000%',
       'background-position-x': this.countries[this.country].col * -100 + '%',
       'background-position-y': this.countries[this.country].row * -100 + '%'
     }
