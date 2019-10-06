b0fd5c1efbe18ceb6a98a81a16c989b4e10d932d
Zienit <4760167+zienit@users.noreply.github.com>
2019-10-06 23:17:06 +0200

    change sessions

--- src/app/event/event-edit/event-edit.component.ts
+++ src/app/event/event-edit/event-edit.component.ts
@@ -6,6 +6,7 @@ import { map } from 'rxjs/operators';
 import { SessionType, GrandPrix } from '../event.model';
 import { EventPreviewComponent } from '../event-preview/event-preview.component';
 import { MatDialog } from '@angular/material/dialog';
+import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-event-edit',
@@ -13,8 +14,6 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./event-edit.component.css']
 })
 export class EventEditComponent implements OnInit {
-
-  readonly sessionTypes = SessionType;
 
   constructor(
     private route: ActivatedRoute,
@@ -24,47 +23,56 @@ export class EventEditComponent implements OnInit {
   ) {
   }
 
+  sessions = new FormArray([]);
+
   form = new FormGroup({
     name: new FormControl(null),
     country: new FormControl(null),
     timezone: new FormControl(null),
-    sessions: new FormArray([])
+    sessions: this.sessions
   });
+
+  missingSessionTypes: SessionType[] = [];
 
   ngOnInit() {
     this.route.paramMap.pipe(
       map(paramMap => this.eventService.getGrandPrix(paramMap.get('id')))
-    ).subscribe(e => {
-      e.sessions.forEach(s => (this.form.get('sessions') as FormArray).push(this.createSessionForm()));
-      this.form.setValue({
-        name: e.name,
-        country: e.country,
-        timezone: e.timezone,
-        sessions: e.sessions.map(s => {
-          return {
-            type: s.type,
-            date: new Date(Date.UTC(s.date.getUTCFullYear(), s.date.getUTCMonth(), s.date.getUTCDate())),
-            time: s.date.getUTCHours() + ':' + ('0' + s.date.getUTCMinutes()).slice(-2)
-          }
-        })
-      });
+    ).subscribe(gp => {
+      this.updateForm(gp);
+      this.missingSessionTypes = this.findMissingSessionTypes();
+    });
+  }
+
+  private updateForm(gp: GrandPrix) {
+    gp.sessions.forEach(s => this.sessions.push(this.createSessionForm()));
+    this.form.setValue({
+      name: gp.name,
+      country: gp.country,
+      timezone: gp.timezone,
+      sessions: gp.sessions.map(s => {
+        return {
+          type: s.type,
+          date: new Date(Date.UTC(s.date.getUTCFullYear(), s.date.getUTCMonth(), s.date.getUTCDate())),
+          time: s.date.getUTCHours() + ':' + ('0' + s.date.getUTCMinutes()).slice(-2)
+        }
+      })
     });
   }
 
   createSessionForm() {
     return new FormGroup({
       type: new FormControl(null),
-      date: new FormControl(null),
-      time: new FormControl(null, Validators.pattern('\\d{1,2}:\\d{2}'))
+      date: new FormControl(null, Validators.required),
+      time: new FormControl(null, [Validators.required, Validators.pattern('\\d{1,2}:\\d{2}')])
     });
   }
 
   onSubmit() {
-    const gp = this.getGrandprix()
+    const gp = this.readForm()
     console.log(gp);
   }
 
-  private getGrandprix(): GrandPrix {
+  private readForm(): GrandPrix {
     const value = this.form.value;
     return {
       id: null,
@@ -82,8 +90,37 @@ export class EventEditComponent implements OnInit {
   }
 
   onPreview() {
-    const dialogRef = this.dialog.open(EventPreviewComponent, {
-      data: { grandprix: this.getGrandprix() }
+    this.dialog.open(EventPreviewComponent, {
+      data: { grandprix: this.readForm() }
     });
+  }
+
+  onDeleteSession(index: number) {
+    this.sessions.removeAt(index);
+    this.missingSessionTypes = this.findMissingSessionTypes();
+  }
+
+  onInsertSession(sessionType: SessionType) {
+    const sessionForm = this.createSessionForm();
+    sessionForm.get('type').setValue(sessionType);
+    const index = this.findSessionTypeInsertionIndex(sessionType);
+    if (index === -1) {
+      this.sessions.push(sessionForm);
+    } else {
+      this.sessions.insert(index, sessionForm);
+    }
+    this.missingSessionTypes = this.findMissingSessionTypes();
+  }
+
+  private findMissingSessionTypes() {
+    const usedSessionTypes = this.sessions.controls
+      .map(c => c.get('type').value);
+    return [SessionType.FP1, SessionType.FP2, SessionType.FP3, SessionType.Q1, SessionType.R]
+      .filter(t => !usedSessionTypes.includes(t));
+  }
+
+  private findSessionTypeInsertionIndex(sessionType: SessionType) {
+    return this.sessions.controls
+      .findIndex(c => c.get('type').value > sessionType);
   }
 }
